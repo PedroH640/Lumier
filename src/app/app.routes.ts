acad325486810a63cc66144d66acf196c3ff055d
PedroH640 <pet0159105@aluno.firjansenaisesi.com.br>
2026-06-22 16:40:13 -0300

    Lumière

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './paginas/inicio-component/inicio-component';
+import { CadastroComponent } from './paginas/cadastro-component/cadastro-component';
+import { LoginComponent } from './paginas/login-component/login-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: "inicio", component: InicioComponent},
+    {path: "", pathMatch: 'full', redirectTo: 'inicio'},
+
+    {path: "cadastro", component: CadastroComponent},
+    
+    {path: "login", component: LoginComponent},
+   
+];
+
+
+
---
+++ src/app/paginas/inicio-component/inicio-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RoupasComponent } from '../../roupas-component/roupas-component';
+
+@Component({
+  selector: 'app-inicio-component',
+  imports: [RoupasComponent],
+  templateUrl: './inicio-component.html',
+  styleUrl: './inicio-component.css',
+})
+export class InicioComponent {}
